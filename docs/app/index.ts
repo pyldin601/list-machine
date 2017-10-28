6508cef61471b60cf40fe47c8bc47922131f5eb7
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-10-28 22:17:17 +0300

    Setup REPL project

---
+++ docs/app/index.ts
@@ -0,0 +1,10 @@
+import * as Terminal from 'xterm';
+import './index.css';
+
+(Terminal as any).loadAddon('fit');
+
+const xterm = new Terminal();
+
+xterm.open(document.getElementById('xterm'));
+xterm.write('Hello from Terminal');
+xterm.fit();
---
+++ docs/webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: path.resolve('./docs/app/index.ts'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve('./docs/assets'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader?configFile=docs/tsconfig.json',
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        }),
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: ({resource}) => /node_modules/.test(resource),
+    }),
+    new ExtractTextPlugin("[name].style.css"),
+  ],
+};
