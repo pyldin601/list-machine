cc3d85a6d5f35c2bc15f4c5b82ee71dd68d58a16
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-10-28 23:52:06 +0300

    Empty REPL console

--- docs/app/components/Console.tsx
+++ docs/app/components/Console.tsx
@@ -1,7 +1,55 @@
 import * as React from 'react';
 
-export default class Console extends React.Component<{}, {}> {
+interface IConsoleStateInterface {
+  history: string[],
+  input: string,
+}
+
+export default class Console extends React.Component<{}, IConsoleStateInterface> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      history: [],
+      input: '',
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onKeyPressed = this.onKeyPressed.bind(this);
+  }
+
   public render() {
-    return null;
+    return (
+      <div className="console">
+        {this.renderHistory()}
+        <input
+          className="input"
+          onChange={this.onInputChange}
+          onKeyPress={this.onKeyPressed}
+          value={this.state.input}
+        />
+      </div>
+    );
+  }
+
+  private onInputChange(event: React.ChangeEvent<any>) {
+    this.setState({ input: event.target.value });
+  }
+
+  private onKeyPressed(event: React.KeyboardEvent<any>) {
+    if (event.key === 'Enter') {
+      this.setState({
+        history: [...this.state.history, this.state.input],
+        input: '',
+      });
+    }
+  }
+
+  private renderHistory() {
+    return <div className="history">
+      {this.state.history.map((row, index) => (
+        <div className="row">{row}</div>
+      ))}
+    </div>;
   }
 }
