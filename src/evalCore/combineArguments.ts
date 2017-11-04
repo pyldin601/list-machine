dd88071d1193cc736a3c49c52b29378500f999bf
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-11-04 13:01:12 +0200

    Add recursive arguments destructuring

--- src/evalCore/combineArguments.ts
+++ src/evalCore/combineArguments.ts
@@ -1,13 +1,28 @@
 import * as _ from 'lodash';
 import optimizeTailCall from '../optimizeTailCall';
 import { SPREST } from "../special";
-import LMSymbol from "../types/LMSymbol";
+import { isLMSymbol, LMSymbol } from "../types/";
 import { isList } from "../util";
 
 const STATE_INITIAL = 0;
 const STATE_AFTER_REST = 2;
 
-export default (argValues: any[], argNames: any[]) => {
+const isSprestOperator = (exp: any): boolean => {
+  return isList(exp)
+    && _.size(exp) === 2
+    && exp.every(isLMSymbol)
+    && _.head<LMSymbol>(exp).value === SPREST;
+};
+
+const getArgumentOfSprestOperator = (exp: any): LMSymbol => {
+  return _.last<LMSymbol>(exp);
+};
+
+const combineArguments = (argValues: any[], argNames: any[]) => {
+  if (!isList(argValues)) {
+    throw new Error(`Argument combiner requires list of values`);
+  }
+
   const packIterate = optimizeTailCall((restArgValues: any[], restArgNames: any[], state: number, acc: {}): {} => {
     if (_.includes([STATE_INITIAL, STATE_AFTER_REST], state) && _.isEmpty(restArgNames)) {
       return acc;
@@ -15,18 +30,27 @@ export default (argValues: any[], argNames: any[]) => {
 
     switch (state) {
       case STATE_AFTER_REST:
-        throw new Error('Rest argument should be last');
+        throw new Error('Sprest argument should be last');
 
       case STATE_INITIAL: {
         const [argName] = restArgNames;
 
-        if (isList(argName) && _.head<LMSymbol>(argName).value === SPREST) {
+        if (isSprestOperator(argName)) {
           return packIterate(
             [],
             _.tail(restArgNames),
             STATE_AFTER_REST,
-            { ...acc, [_.last<LMSymbol>(argName).value]: restArgValues },
+            { ...acc, [getArgumentOfSprestOperator(argName).value]: restArgValues },
           );
+        }
+
+        if (isList(argName)) {
+          return packIterate(
+            _.tail(restArgValues),
+            _.tail(restArgNames),
+            STATE_INITIAL,
+            { ...acc, ...combineArguments(_.head(restArgValues), argName) },
+          )
         }
 
         return packIterate(
@@ -41,3 +65,5 @@ export default (argValues: any[], argNames: any[]) => {
 
   return packIterate(argValues, argNames, STATE_INITIAL, {});
 };
+
+export default combineArguments;
