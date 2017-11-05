94f4c8198740c1c8157be185cd744f344c84c393
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-11-05 15:43:02 +0200

    Make parser based on generators

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,92 +1,103 @@
+import { toArray } from 'lodash';
 import { INode, IToken } from "./types";
 
 const parseExpression = (tokens: IterableIterator<IToken>, type: INode['type']): INode => {
-  const body: INode[] = [];
-
-  for (const token of tokens) {
-    switch (token.type) {
-      case 'Punctuator':
-        switch (token.value) {
-          case 'LeftParen':
-            body.push(parseExpression(tokens, 'ListExpression'));
-            break;
-
-          case 'LeftBracket':
-            body.push(parseExpression(tokens, 'BracketExpression'));
-            break;
-
-          case 'RightParen':
-            if (type !== 'ListExpression') {
-              throw new Error('Unexpected token');
-            }
-            return { body, type };
-
-          case 'RightBracket':
-            if (type !== 'BracketExpression') {
-              throw new Error('Unexpected token');
-            }
-            return { body, type };
-
-          case 'Space':
-          case 'LineFeed':
-          case 'Tab':
-            break;
-
-          default:
-            throw new Error(`Unexpected punctuator - ${token.value}`);
-        }
-        break;
-
-      case 'Boolean':
-        body.push({
-          raw: token.value,
-          type: 'Literal',
-          value: token.value === 'true',
-        });
-        break;
-
-      case 'Number':
-        body.push({
-          raw: token.value,
-          type: 'Literal',
-          value: parseFloat(token.value),
-        });
-        break;
-
-      case 'String':
-        body.push({
-          raw: token.value,
-          type: 'Literal',
-          value: token.value,
-        });
-        break;
-
-      case 'RegExp':
-        body.push({
-          raw: token.value,
-          type: 'Literal',
-          value: new RegExp(token.value),
-        });
-        break;
-
-      case 'Id':
-        body.push({
-          name: token.value,
-          raw: token.value,
-          type: 'Id',
-        });
-        break;
-
-      default:
-        throw new Error(`Unexpected token - ${token.type}`);
-    }
-  }
-
-  if (type !== 'RootExpression') {
-    throw new Error('Unexpected token');
-  }
+  const body = toArray(parseToken(tokens, type) as any);
 
   return { type, body };
 };
+
+function* parseToken (tokens: IterableIterator<IToken>, type: INode['type']): IterableIterator<INode> {
+  const nextToken = tokens.next();
+
+  if (nextToken.done) {
+    if (type !== 'RootExpression') {
+      throw new Error('Unexpected EOF');
+    }
+
+    return;
+  }
+
+  const token = nextToken.value;
+
+  switch (token.type) {
+    case 'Punctuator':
+      switch (token.value) {
+        case 'LeftParen':
+          yield parseExpression(tokens, 'ListExpression');
+          break;
+
+        case 'LeftBracket':
+          yield parseExpression(tokens, 'BracketExpression');
+          break;
+
+        case 'RightParen':
+          if (type !== 'ListExpression') {
+            throw new Error(`Unexpected punctuator value - ${token.value}`);
+          }
+          return;
+
+        case 'RightBracket':
+          if (type !== 'BracketExpression') {
+            throw new Error(`Unexpected punctuator value - ${token.value}`);
+          }
+          return;
+
+        case 'Space':
+        case 'LineFeed':
+        case 'Tab':
+          break;
+
+        default:
+          throw new Error(`Unexpected punctuator - ${token.value}`);
+      }
+      break;
+
+    case 'Boolean':
+      yield {
+        raw: token.value,
+        type: 'Literal',
+        value: token.value === 'true',
+      };
+      break;
+
+    case 'Number':
+      yield {
+        raw: token.value,
+        type: 'Literal',
+        value: parseFloat(token.value),
+      };
+      break;
+
+    case 'String':
+      yield {
+        raw: token.value,
+        type: 'Literal',
+        value: token.value,
+      };
+      break;
+
+    case 'RegExp':
+      yield {
+        raw: token.value,
+        type: 'Literal',
+        value: new RegExp(token.value),
+      };
+      break;
+
+    case 'Id':
+      yield {
+        name: token.value,
+        raw: token.value,
+        type: 'Id',
+      };
+      break;
+
+    default:
+      throw new Error(`Unexpected token - ${token.type}`);
+  }
+
+  yield * parseToken(tokens, type);
+}
 
 export default (tokens: IterableIterator<IToken>) => parseExpression(tokens, 'RootExpression');
